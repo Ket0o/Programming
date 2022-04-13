fac86e5b30418130e6251ccba5d12dfd2a14c180
Ket0o <kupregion3@gmail.com>
2022-04-13 21:52:36 +0700

    25. Поле _id должно инициализироваться в конструкторе (или конструкторах) класса значением из поля _allRectanglesCount. Поскольку статическое поле _allRectanglesCount в конструкторе каждый раз увеличивается на единицу, то у каждого объекта класса Rectangle должен быть свой уникальный Id, каждый раз больше на единицу, чем у ранее созданного объекта.

--- Programming/Model/Classes/Rectangle.cs
+++ Programming/Model/Classes/Rectangle.cs
@@ -8,9 +8,21 @@ namespace Programming.Model.Classes
 {
     public class Rectangle
     {
+        private static int _allRectanglesCount;
+
         private double _length;
 
         private double _width;
+
+        private int _id;
+
+        public static int AllRectanglesCount
+        {
+            get
+            {
+                return _allRectanglesCount;
+            }
+        }
 
         public double Length
         {
@@ -38,11 +50,23 @@ namespace Programming.Model.Classes
             }
         }
 
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+        }
+
         public Point2D Center { get; set; }
 
         public string Color { get; set; }
 
-        public Rectangle() { }
+        public Rectangle() 
+        {
+            _allRectanglesCount++;
+            _id = _allRectanglesCount;
+        }
 
         public Rectangle(double length, double width, string color, Point2D center)
         {
@@ -50,6 +74,8 @@ namespace Programming.Model.Classes
             Width = width;
             Color = color;
             Center = center;
+            _allRectanglesCount++;
+            _id = _allRectanglesCount;
         }
     }
 }
