38117910c200b55e392bbfa0a64104de2200f5ee
Ket0o <kupregion3@gmail.com>
2022-05-28 12:03:01 +0700

    Json

---
+++ ListOfEmployees/Model/Classes/ProjectSerializer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using ListOfEmployees.Model.Employees;
+using Newtonsoft.Json;
+
+
+namespace ListOfEmployees.Model.Classes
+{
+    public class ProjectSerializer
+    {
+        public void Serialize(List<Employee> employees)
+        {
+            using (StreamWriter sw = new StreamWriter(@"Serializer.json"))
+            {
+                sw.Write(JsonConvert.SerializeObject(employees));
+            }
+        }
+
+        public List<Employee> Deserialize()
+        {
+            var employees = new List<Employee>();
+
+            using(StreamReader sr = new StreamReader(@"Serializer.json"))
+            {
+                employees = JsonConvert.DeserializeObject<List<Employee>>(sr.ReadToEnd());
+                if(employees == null)
+                {
+                    employees = new List<Employee>();
+                }
+            }
+
+            return employees;
+        }
+    }
+}
