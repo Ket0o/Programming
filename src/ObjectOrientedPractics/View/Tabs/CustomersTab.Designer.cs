292d1f65a1d0d686918ca5e59764eb8bc1f1fda8
Ket0o <kupregion3@gmail.com>
2022-10-01 14:21:49 +0700

    123

--- src/ObjectOrientedPractics/View/Tabs/CustomersTab.Designer.cs
+++ src/ObjectOrientedPractics/View/Tabs/CustomersTab.Designer.cs
@@ -39,17 +39,17 @@
             this.AddButton = new System.Windows.Forms.Button();
             this.ListBoxCustomers = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // AddressTextBox
             // 
             this.AddressTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.AddressTextBox.Location = new System.Drawing.Point(563, 149);
+            this.AddressTextBox.Location = new System.Drawing.Point(383, 148);
+            this.AddressTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.AddressTextBox.Multiline = true;
             this.AddressTextBox.Name = "AddressTextBox";
-            this.AddressTextBox.Size = new System.Drawing.Size(416, 224);
+            this.AddressTextBox.Size = new System.Drawing.Size(514, 184);
             this.AddressTextBox.TabIndex = 30;
             this.AddressTextBox.TextChanged += new System.EventHandler(this.AddressTextBox_TextChanged);
             // 
@@ -57,36 +57,39 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(379, 124);
+            this.label4.Location = new System.Drawing.Point(379, 125);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(72, 20);
+            this.label4.Size = new System.Drawing.Size(48, 13);
             this.label4.TabIndex = 27;
             this.label4.Text = "Address:";
             // 
             // FullNameTextBox
             // 
-            this.FullNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.FullNameTextBox.Location = new System.Drawing.Point(581, 116);
+            this.FullNameTextBox.Location = new System.Drawing.Point(466, 82);
+            this.FullNameTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.FullNameTextBox.Name = "FullNameTextBox";
-            this.FullNameTextBox.Size = new System.Drawing.Size(428, 26);
+            this.FullNameTextBox.Size = new System.Drawing.Size(286, 20);
             this.FullNameTextBox.TabIndex = 26;
             this.FullNameTextBox.TextChanged += new System.EventHandler(this.FullNameTextBox_TextChanged);
             // 
             // IdTextBox
             // 
-            this.IdTextBox.Location = new System.Drawing.Point(469, 44);
+            this.IdTextBox.Location = new System.Drawing.Point(466, 42);
+            this.IdTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.IdTextBox.Name = "IdTextBox";
             this.IdTextBox.ReadOnly = true;
-            this.IdTextBox.Size = new System.Drawing.Size(174, 26);
+            this.IdTextBox.Size = new System.Drawing.Size(118, 20);
             this.IdTextBox.TabIndex = 25;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(379, 84);
+            this.label5.Location = new System.Drawing.Point(379, 85);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(84, 20);
+            this.label5.Size = new System.Drawing.Size(57, 13);
             this.label5.TabIndex = 24;
             this.label5.Text = "Full Name:";
             // 
@@ -94,9 +97,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(379, 44);
+            this.label3.Location = new System.Drawing.Point(379, 45);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(30, 20);
+            this.label3.Size = new System.Drawing.Size(21, 13);
             this.label3.TabIndex = 23;
             this.label3.Text = "ID:";
             // 
@@ -104,16 +108,18 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(379, 9);
+            this.label2.Location = new System.Drawing.Point(379, 10);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(162, 20);
+            this.label2.Size = new System.Drawing.Size(113, 13);
             this.label2.TabIndex = 22;
             this.label2.Text = "Selected Customer";
             // 
             // RemoveButton
             // 
             this.RemoveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.RemoveButton.Location = new System.Drawing.Point(157, 784);
+            this.RemoveButton.Location = new System.Drawing.Point(131, 642);
+            this.RemoveButton.Margin = new System.Windows.Forms.Padding(2);
             this.RemoveButton.Name = "RemoveButton";
             this.RemoveButton.Size = new System.Drawing.Size(118, 54);
             this.RemoveButton.TabIndex = 21;
@@ -124,7 +130,8 @@
             // AddButton
             // 
             this.AddButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.AddButton.Location = new System.Drawing.Point(8, 784);
+            this.AddButton.Location = new System.Drawing.Point(7, 642);
+            this.AddButton.Margin = new System.Windows.Forms.Padding(2);
             this.AddButton.Name = "AddButton";
             this.AddButton.Size = new System.Drawing.Size(118, 54);
             this.AddButton.TabIndex = 20;
@@ -137,10 +144,10 @@
             this.ListBoxCustomers.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
             this.ListBoxCustomers.FormattingEnabled = true;
-            this.ListBoxCustomers.ItemHeight = 20;
-            this.ListBoxCustomers.Location = new System.Drawing.Point(7, 32);
+            this.ListBoxCustomers.Location = new System.Drawing.Point(7, 33);
+            this.ListBoxCustomers.Margin = new System.Windows.Forms.Padding(2);
             this.ListBoxCustomers.Name = "ListBoxCustomers";
-            this.ListBoxCustomers.Size = new System.Drawing.Size(439, 724);
+            this.ListBoxCustomers.Size = new System.Drawing.Size(366, 602);
             this.ListBoxCustomers.TabIndex = 19;
             this.ListBoxCustomers.SelectedIndexChanged += new System.EventHandler(this.ListBoxCustomers_SelectedIndexChanged);
             // 
@@ -148,24 +155,16 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Location = new System.Drawing.Point(3, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.Size = new System.Drawing.Size(65, 13);
             this.label1.TabIndex = 18;
             this.label1.Text = "Customers";
             // 
-            // panel2
-            // 
-            this.panel2.Location = new System.Drawing.Point(383, 286);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(514, 411);
-            this.panel2.TabIndex = 31;
-            // 
             // CustomersTab
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.Controls.Add(this.panel2);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
             this.Controls.Add(this.AddressTextBox);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.FullNameTextBox);
@@ -177,8 +176,9 @@
             this.Controls.Add(this.AddButton);
             this.Controls.Add(this.ListBoxCustomers);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "CustomersTab";
-            this.Size = new System.Drawing.Size(771, 598);
+            this.Size = new System.Drawing.Size(904, 699);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -196,6 +196,5 @@
         private System.Windows.Forms.Button AddButton;
         private System.Windows.Forms.ListBox ListBoxCustomers;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Panel panel2;
     }
 }
