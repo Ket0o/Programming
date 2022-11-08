3f38a57c9294b973d7649371f56b13719cb7e09b
Ket0o <kupregion3@gmail.com>
2022-11-08 16:16:22 +0700

    123

--- src/ObjectOrientedPractics/View/Tabs/OrdersTab.cs
+++ src/ObjectOrientedPractics/View/Tabs/OrdersTab.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ObjectOrientedPractics.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,65 @@ namespace ObjectOrientedPractics.View.Tabs
 {
     public partial class OrdersTab : UserControl
     {
+        /// <summary>
+        /// Коллекция товаров.
+        /// </summary>
+        private List<Item> _items;
+
+        /// <summary>
+        /// Коллекция покупателей.
+        /// </summary>
+        private List<Customer> _customers;
+
+        private List<Order> _orders;
+
+        private Order _currentOrder;
+
         public OrdersTab()
         {
             InitializeComponent();
+
+            _orders = new List<Order>();
+
+            var orderStatusValues = Enum.GetValues(typeof(OrderStatus));
+
+            foreach (var value in orderStatusValues)
+            {
+                StatusComboBox.Items.Add(value);
+            }
+
+            StatusComboBox.Enabled = false;
+        }
+
+        public List<Customer> Customers
+        {
+            get { return _customers; }
+            set
+            {
+                _customers = value;
+
+                if (_customers != null)
+                {
+                    UpdateOrderInfo();
+                }
+            }
+        }
+
+        private void UpdateOrderInfo()
+        {
+            foreach (var customer in _customers)
+            {
+                Address address = customer.Address;
+                string fullAddress = $"{address.Index}, {address.Country}, {address.City}," +
+                                     $" {address.Street}, {address.Building}, {address.Apartment}";
+
+                foreach (var order in customer.Orders)
+                {
+                    _orders.Add(order);
+                    //TODO:.Rows.Add(order.Id.ToString(), order.DateOfCreate,
+                        order.Status, customer.Fullname, fullAddress, order.Amount.ToString());
+                }
+            }
         }
     }
 }
