a7fd8db1c693eb5fa68d1bff66989bf08bd1c925
Ket0o <kupregion3@gmail.com>
2023-04-07 15:40:10 +0700

    123

--- src/WpfContacts/View/Windows/MainWindow.xaml.cs
+++ src/WpfContacts/View/Windows/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfContacts
+namespace WpfContacts.View.Windows
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
