b5cc5c8bf57c1205bcead37d6e22923d4d0204a6
Ket0o <kupregion3@gmail.com>
2023-05-17 01:37:27 +0700

    123

--- src/WpfContacts/Model/Services/MaxDigitValidationRule.cs
+++ src/WpfContacts/Model/Services/MaxDigitValidationRule.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace WpfContacts.Model.Services
 {
-    internal class MaxDigitValidationRule
+    public class MaxDigitValidationRule: ValidationRule
     {
     }
 }
--- src/WpfContacts/ViewModel/ContactVM.cs
+++ src/WpfContacts/ViewModel/ContactVM.cs
@@ -7,14 +7,14 @@ namespace WpfContacts.ViewModel
     /// <summary>
     /// ViewModel для contact.
     /// </summary>
-    public class ContactVM : Base.ViewModel, IDataErrorInfo
+    public class ContactVM : Base.ViewModel
     {
         /// <summary>
         /// Контактные данные.
         /// </summary>
         public Contact Contact { get; } = new Contact();
 
-        /// <summary>
+        /// <summary>   
         /// Возвращает и задает имя.
         /// </summary>
         public string Name
@@ -53,45 +53,5 @@ namespace WpfContacts.ViewModel
             }
         }
 
-        public string this[string columnName]
-        {
-            get
-            {
-                string error = String.Empty;
-                switch (columnName)
-                {
-                    case "Name":
-                        if (Name == null)
-                        {
-                            break;
-                        }
-                        if (Name.Length > 100)
-                        {
-                            error = "Name должен быть не длиннее 100 символов";
-                        }
-                        break;
-                    case "PhoneNumber":
-                        if (PhoneNumber == null)
-                        {
-                            break;
-                        }
-
-                        if (PhoneNumber.Length > 100)
-                        {
-                            error =
-                                "PhoneNumber должен быть не длиннее 100 символов и " +
-                                "может содержать только цифры или символы +-() .\r\nПример: " +
-                                "+7 (999) 111-22-33\r\n";
-                        }
-                        break;
-                }
-                return error;
-            }
-        }
-
-        public string Error
-        {
-            get { throw new NotImplementedException(); }
-        }
     }
 }
